faa3aeca9bba06ade67352fa13466636ff0e7372
Nykolle Malone <nykolle.malone@hotmail.com>
2020-01-11 08:36:14 -0300

    fixing some setup issues, starting layout, starting first routes (sing up and login)

--- code/app.js
+++ code/app.js
@@ -1,18 +1,51 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+const express = require("express");
+const hbs = require("hbs");
+const mongoose = require('mongoose');
+const path = require("path");
+const User = require('./models/user');
+const Place = require('./models/place');
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy; 
+const bcrypt = require("bcrypt");
+const flash = require("connect-flash");
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+  })
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
 
 const app = express();
 
+// Middleware Setup
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
+// Express View engine setup
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const index = require('./routes/index');
 
-app.use('/', index);
+const index = require("./routes/index");
+app.use("/", index);
 
-app.listen(process.env.PORT, () => console.log(`rodando na porta ${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`rodando na porta ${process.env.PORT}`)
+);
---
+++ code/routes/auth.js
@@ -0,0 +1,72 @@
+// routes/auth.js
+const express = require("express");
+const router = express.Router();
+// User model
+const User = require("../models/user");
+const Course = require("../models/course");
+// BCrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+// adding passport for auth routes
+const passport = require("passport");
+
+//________________________________________________________SIGN UP___________________________________________________________//
+//GET
+router.get("/signup", (req, res, next) => { 
+  res.render("auth/signup");
+});
+
+//POST
+router.post("/signup", (req, res, next) => {
+  const {
+    username,
+    password,
+  } = req.body;
+
+  //CHECKING CONTENT
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  //CHEKING PASSWORD LENGTH
+  if (password.length < 6) {
+    res.render("auth/signup", {
+      errorMessage: "Password shoul be at least 6 characters"
+    });
+    return;
+  }
+
+  //CHEKING USERNAME
+  User
+  .findOne({username})
+  .then(user => {
+    if (user) {
+      res.render("auth/signup", {
+          errorMessage: "The username already exists!"
+    });
+        return;
+  }
+
+  //BCRYPTING PASSWORD
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(password, salt);
+
+  //CREATING USER
+  User
+  .create({ username, password: hashPass })
+  .then(() => {
+    res.redirect("/");
+  })
+  .catch(error => {
+    console.log(error);
+  })
+})
+  .catch(error => {next(error);
+  })
+});
+
+
+module.exports = router;
