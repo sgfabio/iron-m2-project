90256ef16b21459168aec2ef90f5885bc1297b5e
Nykolle Malone <nykolle.malone@hotmail.com>
2020-01-13 12:28:07 -0300

    Destructuring mongoose schema and model

--- code/models/place.js
+++ code/models/place.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const { Schema, model} = mongoose;
 
 const placeSchema = new Schema({
   name: String,
@@ -31,6 +31,6 @@ const placeSchema = new Schema({
 
 placeSchema.index({ location: "2dsphere" })
 
-const Place = mongoose.model("Place", placeSchema);
+const Place = model("Place", placeSchema);
 
 module.exports = Place;
